833c9a8aa19210e892440f734bac20b4997cc82d
Daniel Rauer <rauer@bytemine.net>
2016-11-13 23:32:11 +0100

    Initial commit, working revision.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_default_assignee_is_me do
+  name 'Redmine Default Assignee Is Me plugin'
+  author 'Daniel Rauer, bytemine GmbH'
+  description 'Redmine plugin to set the assignee to oneself if not otherwise set'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'https://www.bytemine.net'
+end
